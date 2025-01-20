46b87895b9736e0a794de69b0221968f7e530307
“Stanislav” <stanyyaro@gmail.com>
2025-01-20 22:24:02 +0100

    U2-W3-D1-HM

---
+++ assets/js/scrypt.js
@@ -0,0 +1,22 @@
+class User {
+  constructor(firstName, lastName, age, location) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.location = location;
+  }
+  confronto(other) {
+    if (this.age > other.age) {
+      console.log(`${this.firstName} è più grande di ${other.firstName}`);
+    } else if (this.age === other.age) {
+      console.log(`${this.firstName} è uguale a ${other.firstName}`);
+    } else {
+      console.log(`${this.firstName} è più piccolo di ${other.firstName}`);
+    }
+  }
+}
+
+const utentex = new User("valentino", "rossi", 45, "monza");
+const utentey = new User("Giovanni", "D'Anna", 26, "Cirignano");
+
+utentex.confronto(utentey);
